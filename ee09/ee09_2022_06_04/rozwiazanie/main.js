90f82f6cfffed38f69605d8c1acc599a1044511b
Chr1skyy <tofucek@gmail.com>
2024-01-07 16:27:38 +0100

    zmniejszenie wagi calego repo poprzez usuniecie screenow z wykonan kwerend

--- ee09/ee09_2022_06_04/rozwiazanie/main.js
+++ ee09/ee09_2022_06_04/rozwiazanie/main.js
@@ -1,3 +1,37 @@
+/* Dla początkujących */
+function sprawdz2() {
+    const produkty = ['p1', 'p2', 'p3', 'p4']
+    for (let i = 0; i < produkty.length; i++) {
+        let ilosc = document.getElementById(produkty[i]).innerHTML
+        let produkt = document.getElementById(produkty[i])
+        if (ilosc == 0) {
+            produkt.style = 'background-color: red;'
+        }
+        if (ilosc >= 1 && ilosc <= 5) {
+            produkt.style = 'background-color: yellow;'
+        }
+        if (ilosc > 5) {
+            produkt.style = 'background-color: Honeydew;'
+        }
+    }
+}
+
+function aktualizuj(produktID) {
+    const produkt = document.getElementById(produktID)
+    const nowaIlosc = prompt('Podaj nową ilość:')
+    produkt.innerHTML = nowaIlosc
+    sprawdz2()
+}
+
+let idZamowienia = 0
+function zamow(nazwaProduktu) {
+    alert('Zamówienie nr: ' + idZamowienia + ' Produkt: ' + nazwaProduktu)
+    idZamowienia++
+}
+sprawdz2()
+//*/
+
+/* Dla kumatych *
 const iloscProduktow = document.querySelectorAll('.ilosc')
 const nazwaProduktow = document.querySelectorAll('.produkt')
 const aktualizujPrzycisk = document.querySelectorAll('.aktualizuj')
@@ -33,3 +67,4 @@ zamowPrzycisk.forEach((przycisk, i) => {
     })
 })
 sprawdz()
+//*/
